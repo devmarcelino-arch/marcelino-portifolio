70849af7d97e70f77e51d43ea23bb3feebeccca1
devmarcelino-arch <marcelinoomar230@gmail.com>
2026-05-31 20:40:59 +0200

    corrige estrutura swiper

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -44,7 +44,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
 
     <!-- Vite -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/js/app.js', 'resources/css/app.css'])
 </head>
 <body>
 
